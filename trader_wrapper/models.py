3f42af92f00ba48e93811e9d77f96ebcc80f9e79
chapkovski <chapkovski@gmail.com>
2021-05-05 23:46:27 +0300

    some updates before mocking

--- trader_wrapper/models.py
+++ trader_wrapper/models.py
@@ -10,6 +10,7 @@ from otree.api import (
 )
 import random
 from django.db import models as djmodels
+from datetime import datetime, timedelta
 
 author = 'Philipp Chapkovski, HSE-Moscow'
 
@@ -28,7 +29,9 @@ class Constants(BaseConstants):
     name_in_url = 'trader_wrapper'
     players_per_group = None
     num_rounds = 1
-
+    trading_day_duration = 5  # in minutes
+    stocks = ['A', 'B', 'ETF_A', 'ETF_B']
+    endowment = 100
     EVENT_TYPES = AttrDict(
         price_update='PRICE_UPDATE',
         tab_change='TAB_CHANGE',
@@ -40,12 +43,12 @@ class Constants(BaseConstants):
 
 class Subsession(BaseSubsession):
     def creating_session(self):
-        from .utils import creating_events
         for p in self.get_players():
-            creating_events(p)
-            p.age = random.randint(18, 100)
-            p.gender = random.choice(['Male', 'Female'])
-            p.income = random.randint(0, 7)
+            p.generate_deposit()  # TODO: in production move to bulk update right here
+            p.generate_prices()  # TODO: in production move to bulk update right here
+            p.start_time = datetime.now()
+            p.end_time = p.start_time + timedelta(minutes=Constants.trading_day_duration)
+            p.endowment = Constants.endowment  # we may randomize it later, let's keep it simple for now.
 
 
 class Group(BaseGroup):
@@ -53,9 +56,67 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    """In production we may not need theses two fields, but it is still useful to have them
+    as natural limits after which the player should proceed to the next trading day.
+    """
+    start_time = djmodels.DateTimeField(null=True, blank=True)
+    end_time = djmodels.DateTimeField(null=True, blank=True)
+    current_stock_shown = models.StringField(doc='registers сurrent stock shown to a player')
+    current_tab = models.StringField(doc='registers current tab the player is it')
+    endowment = models.FloatField(doc='initial amount given to a participant at the beginning of the trading day')
+    balance = models.FloatField(doc='to store the current state of bank account')
+    ########### BLOCK: post experimental survey quesitons.  ##############################################################
+    # TODO: move to a separate app in production
     age = models.IntegerField()
     gender = models.StringField()
     income = models.IntegerField()
+
+    ############ END OF: post experimental survey quesitons.  #############################################################
+    def generate_prices(self):
+        """we bulk create price updates here.
+        The trick is that also in production we pre-generate a flow of prices BEFORE the trading day starts
+        with corresponding timestamps. Then we feed them every time we get a price_update request from a client.
+        That we guarantee the fastest possible reaction.
+        """
+
+    def generate_deposit(self):
+        """a temporary solution to generate initial set of stocks to keep track of attainability of purchases/sales.
+        in production: return a bunch of deposit objects so they can created in a huge bulk_create for the entire
+        session, not for a single user, which can make it slower than we want.
+        """
+        for i in Constants.stocks:
+            # TODO: bulk creation of Deposits here
+            pass
+
+
+
+    def latest_timestamp(self):
+        """gives a latest possible timestamp"""
+        # TODO: this one to be used in production to send a signal that session is over
+        pass
+
+    def price_request(self, timestamp):
+        """Returns a dict of recent prices that are earlier than a given timestamp"""
+        most_recent_time_stamp = self.prices.filter(timestamp__lte=timestamp).latest()
+        # we use here the fact N stock prices are pre-created in waves in groups of 4
+        # TODO: somewhere here in production we should check that the trading session is not over
+        return self.prices.filter(timestamp=most_recent_time_stamp).values('name', 'price', 'timestamp')
+
+
+class Price(djmodels.Model):
+    class Meta:
+        get_latest_by = 'timestamp'
+
+    name = models.StringField()
+    price = models.FloatField()
+    timestamp = djmodels.DateTimeField(null=True, blank=True)
+    owner = djmodels.ForeignKey(to=Player, on_delete=djmodels.CASCADE, related_name='prices')
+
+
+class Deposit(djmodels.Model):
+    owner = djmodels.ForeignKey(to=Player, on_delete=djmodels.CASCADE, related_name='deposit')
+    quantity = models.IntegerField()
+    name = models.StringField()
 
 
 class Event(djmodels.Model):
--- trader_wrapper/utils.py
+++ trader_wrapper/utils.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 import logging
-from trader_wrapper.models import Event, Constants, AttrDict
+from trader_wrapper.models import Event, Constants, AttrDict, Player
 from dateparser import parse
 from dateutil.relativedelta import relativedelta
 import random
@@ -10,6 +10,53 @@ logger = logging.getLogger(__name__)
 
 STOCKS = ['A', 'B']
 TABS = ['Trade', 'Work']
+
+import pandas as pd
+import numpy as np
+
+creating_events(p)
+p.age = random.randint(18, 100)
+p.gender = random.choice(['Male', 'Female'])
+p.income = random.randint(0, 7)
+def pp(start, end, n):
+    """Taken from: https://stackoverflow.com/questions/50559078/generating-random-dates-within-a-given-range-in-pandas.
+    Just generate a sorted list of N random timestamps between two dates from two python datetimes"""
+    start_u = pd.Timestamp(start).value // 10 ** 9
+    end_u = pd.Timestamp(end).value // 10 ** 9
+    return sorted(pd.to_datetime(
+        pd.DatetimeIndex((10 ** 9 * np.random.randint(start_u, end_u, n, dtype=np.int64)).view('M8[ns]'))))
+
+
+class MockPlayer:
+    attainable_events = dict(
+        work=['submit_task', 'change_tab'],
+        trade=['change_tab', 'transaction', 'change_stock_tab']
+    )
+
+    def __init__(self, owner, num_events):
+        self.owner = owner
+        self.num_events = num_events
+        start = self.owner.start_time
+        end = self.owner.end_time
+        event_timestamps = pp(start, end, num_events)
+        for i in event_timestamps:
+            self.generate_random_event(i)
+
+    def generate_random_event(self, timestamp):
+
+    def register_event(self, event_name, params, ):
+        pass
+
+    def change_tab(self):
+        tab = [t for t in self.tabs if t != self.player.current_tab][0]
+        self.register_event(event_name='change_tab', params={'tab': tab})
+
+    def transaction(self):
+        self.stock = stock
+        self.price = price
+        self.event_type = Constants.EVENT_TYPES.transaction
+        self.quantity = random.randint(0, 100)
+        self.total_amount = self.quantity * self.price
 
 
 class TabChange(AttrDict):
@@ -21,7 +68,6 @@ class TabChange(AttrDict):
 
 
 class Transaction(AttrDict):
-
     def __init__(self, price, stock, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stock = stock
@@ -32,7 +78,6 @@ class Transaction(AttrDict):
 
 
 class TaskSubmission(AttrDict):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.event_type = Constants.EVENT_TYPES.task_submission
