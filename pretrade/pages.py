04273654d8b6f2349aaa692f96f0f266155a0554
Marius Zoican <m.a.zoican@gmail.com>
2021-07-15 15:42:28 -0400

    Added instructions and comprehension quiz.

--- pretrade/pages.py
+++ pretrade/pages.py
@@ -5,8 +5,11 @@ from ._builtin import Page, WaitPage
 
 class CQPage(Page):
      form_model = 'player'
-     form_fields = ['cq1']
+     form_fields = ['cq1','cq2','cq3','cq4']
+
+class Instructions(Page):
+     pass
 
 
 
-page_sequence = [CQPage]
+page_sequence = [Instructions, CQPage]
