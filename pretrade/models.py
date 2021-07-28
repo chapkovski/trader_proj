162408cf9cad1d0dfcb3ca8552cd80de7c02fa44
Francisco Kahil <codexico@gmail.com>
2021-07-28 17:26:30 +0300

    trad81ready

--- pretrade/models.py
+++ pretrade/models.py
@@ -46,13 +46,13 @@ def general_params(subsession: Subsession):
         "http://raw.githubusercontent.com/chapkovski/trader_proj/main/data/params.yaml").read()
     c = yaml.load(contents, Loader=yaml.FullLoader)
     gps = c.copy()
-    # gps = _general_params.copy() # UNCOMMENT FOR LOCAL testing
+    gps = _general_params.copy() # UNCOMMENT FOR LOCAL testing
     day_params_url ='http://raw.githubusercontent.com/chapkovski/trader_proj/main/data/day_params.csv'
     day_params_req = urllib.request.urlopen(day_params_url).read().decode('utf-8')
     cr = DictReader(day_params_req.splitlines())
 
 
-    _day_params = list(cr) # COMMENt out FOR LOCAL testing
+    # _day_params = list(cr) # COMMENt out FOR LOCAL testing
     numTicks = gps.get('dayLength') / gps.get('tickFrequency')
 
     injected = dict(gamified=subsession.session.config.get('gamified', False),
